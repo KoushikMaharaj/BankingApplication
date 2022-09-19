34a3054e6e1d37c348c3a0043919a08883aefc2d
Koushik Maharaj <koushik.maharaj@hoonartek.com>
2022-09-19 10:58:30 +0530

    modules created

---
+++ BankApplicationApi/src/main/java/bank/BankAppApi.java
@@ -0,0 +1,13 @@
+package bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppApi.class, args);
+	}
+
+}
---
+++ BankApplicationApi/src/test/java/bank/BankAppApiTests.java
@@ -0,0 +1,13 @@
+package bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
